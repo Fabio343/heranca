243c09d6d65df4ac2f6a7791c5de47d69de06c78
php 12 <php.12@jerry.cec.ime.usp.br>
2018-01-29 21:25:43 -0200

    heranca

--- testa_heranca.php
+++ testa_heranca.php
@@ -1,5 +1,6 @@
 <?php
 
+require 'gerente.php';
 $gerente=new gerente();
 
 $gerente->nome="Maria";
@@ -7,10 +8,10 @@ $gerente->matricula=1254;
 $gerente->salario=2500;
 $gerente->senha=357159258;
 
-echo "$gerente->nome";
-echo "$gerente->matricula";
-echo "$gerente->salario";
-echo "$gerente->senha";
+echo "$gerente->nome" . PHP_EOL;
+echo "$gerente->matricula" . PHP_EOL;
+echo "$gerente->salario" . PHP_EOL;
+echo "$gerente->senha" . PHP_EOL;
 
 
  ?>
