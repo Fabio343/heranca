c4ce6f61f038c2b49f6f7c3e7e68a8cb315e7a3c
php 12 <php.12@jerry.cec.ime.usp.br>
2018-01-29 21:23:44 -0200

    heranca

---
+++ testa_heranca.php
@@ -0,0 +1,16 @@
+<?php
+
+$gerente=new gerente();
+
+$gerente->nome="Maria";
+$gerente->matricula=1254;
+$gerente->salario=2500;
+$gerente->senha=357159258;
+
+echo "$gerente->nome";
+echo "$gerente->matricula";
+echo "$gerente->salario";
+echo "$gerente->senha";
+
+
+ ?>
